b07dc4e494d82e9067f044818a974b0e23a7d483
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-01-05 07:35:56 -0800

    add the img

--- react project/myproject/src/components/header.js
+++ react project/myproject/src/components/header.js
@@ -1,3 +1,4 @@
+import web from "../assets/web.png";
 function Header(){
     return (
        <div>
@@ -6,6 +7,7 @@ function Header(){
                deleniti soluta voluptates quia recusandae quaerat quasi?</p>
                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, fugit officiis blanditiis quas impedit accusamus. Alias corrupti id, molestias minus libero eos perspiciatis
                deleniti soluta voluptates quia recusandae quaerat quasi?</p>
+               <img src={web} alt="" />
        </div>
     )
 }
