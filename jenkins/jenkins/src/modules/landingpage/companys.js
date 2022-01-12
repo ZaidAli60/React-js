44b692f13be5e72b484aab1bf06ffc2fa90c5841
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-01-12 08:42:23 -0800

    add image

--- jenkins/jenkins/src/modules/landingpage/companys.js
+++ jenkins/jenkins/src/modules/landingpage/companys.js
@@ -2,6 +2,10 @@ import cloudbees from "./../../assets/cloudbees.png"
 import osl from "./../../assets/osuosl.png"
 import cdf from "./../../assets/cdf.png"
 import redhat from "./../../assets/redhat.png"
+import github from "./../../assets/github.png"
+import jfrog from "./../../assets/jfrog.png"
+import aws from "./../../assets/aws.png"
+import "./company.css"
 
 
 
@@ -10,14 +14,17 @@ function Companys() {
         <div>
             <div className="container">
                 <p className="text-center">We thank the following organizations for their major commitments to support the Jenkins project.</p>
-                <div className="row py-4">
-                    <img className="img-fluid col-lg-3 col-md-4 col-sm-6" src={cloudbees} alt="cloudbees" />
-                    <img className="img-fluid col-lg-3 col-md-4 col-sm-6" src={osl} alt="osl" />
-                    <img className="img-fluid col-lg-3 col-md-4 col-sm-6" src={cdf} alt="cdf" />
-                    <img className="img-fluid col-lg-3 col-md-4 col-sm-6" src={redhat} alt="redhat" />
+                <div className="row py-4 pics">
+                    <img className="pic1 img-fluid col-lg-2 col-md-4 col-sm-6" src={cloudbees} alt="cloudbees" />
+                    <img className=" img-fluid col-lg-2 col-md-4 col-sm-6" src={osl} alt="osl" />
+                    <img className="pic2 img-fluid col-lg-2 col-md-4 col-sm-6" src={cdf} alt="cdf" />
+                    <img className="img-fluid col-lg-2 col-md-4 col-sm-6" src={redhat} alt="redhat" />
+                    <img className="img-fluid col-lg-3 col-md-4 col-sm-6" src={aws} alt="redhat" />
+
                 </div>
-                <div className="row">
-                <img className="img-fluid col-lg-3 col-md-4 col-sm-6" src={redhat} alt="redhat" />
+                <div className="row pics2 ">
+                <img className="img-fluid col-lg-3 col-md-4 col-sm-6" src={github} alt="redhat" />
+                <img className="img-fluid col-lg-3 col-md-4 col-sm-6" src={jfrog} alt="redhat" />
                     
                 </div>
             </div>
