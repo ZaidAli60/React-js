5db3854f49bf5eb05a7039d0454c9037a43e2c78
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-02-08 09:25:21 -0800

    from submit

--- practics/props usestate/practics/src/component/fromHandling.js
+++ practics/props usestate/practics/src/component/fromHandling.js
@@ -3,15 +3,37 @@ import {useState} from 'react';
 
 function FromHandling() {
  const [name ,setName] = useState("");
+ const [password ,setPassword] = useState("");
+ const [Class ,setClass] = useState("");
+ const [section ,setSection] = useState("");
 
- var fromHandling = (e) =>{
-    setName(e.target.value);
-    // console.log(e)
+ const fromSubmit = () =>{
+    const obj = {
+        name,
+        password,
+        Class,
+        section,
+
+    }
+console.log(obj)
+
  }
+
+//  var fromHandling = (e) =>{
+//     setName(e.target.value);
+    // console.log(e)
+//  }
     return(
         <div>
-            <input type="text" onChange={fromHandling} />
+            <input type="text" placeholder='name' onChange={(e)=> setName(e.target.value)} />
+            <input type="password"placeholder='password' onChange={(e)=> setPassword(e.target.value)} />
+            <input type="text" placeholder='Enter your class' onChange={(e)=> setClass(e.target.value)} />
+            <input type="text" placeholder='Enter your section' onChange={(e)=> setSection(e.target.value)} />
+            <button type='submit' onClick={fromSubmit}>Submit</button>
             <p>name is : {name}</p>
+            <p>class is : {Class}</p>
+            <p>section is : {section}</p>
+
         </div>
     )
 }
