a60e252b1cfc4d445d2b64995451595bd5ed433a
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-03-16 12:56:42 -0700

    jenkins slider complete

--- jenkins project/jenkins-project/src/module/landingPage/landingPages.js
+++ jenkins project/jenkins-project/src/module/landingPage/landingPages.js
@@ -1,11 +1,13 @@
 import NavigationBar from "../../commonComponent/navigationBar/navigationBar";
 import Header from "../header/header";
+import Slider from "../slider/slider";
 
 
 function LandingPage() {
     return(<div>
         <NavigationBar />
         <Header />
+        <Slider />
        
     </div>)
 }
---
+++ jenkins project/jenkins-project/src/module/slider/slider.js
@@ -0,0 +1,56 @@
+
+import jenkins from "../../assets/jenkins.png"
+import Carousel from 'react-bootstrap/Carousel'
+import "./slider.css"
+function Slider() {
+    return (
+        <div>
+            <div className="slider-img slider">
+                <Carousel variant="dark">
+                    <Carousel.Item>
+                   <div className="container mt-5">
+                   <div className="row text-light textcontent ">
+                         <div className="col-lg-6">
+                                <h1>Jenkins is way !</h1>
+                                <p className="para">We are looking for experiences of Jenkins <br /> 
+                                   users from around the world showcasing how <br />
+                                    they are building, deploying, and automating <br />
+                                    great software with Jenkins. Check out their <br /> 
+                                    user stories and share yours</p>
+                                    <button className="btn bg-danger text-light">More info</button>
+                         </div>
+                         <div className="col-lg-6">
+                                <img src={jenkins} alt="jenkins" />
+                         </div>
+                     </div>
+                    
+                   </div>
+                    
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      
+                    <div className="container mt-5">
+                    <div className="row text-light textcontent">
+                         <div className="col-lg-6">
+                                <h1>Jenkins is way !</h1>
+                                <p>We are looking for experiences of Jenkins <br /> 
+                                   users from around the world showcasing how <br />
+                                    they are building, deploying, and automating <br />
+                                    great software with Jenkins. Check out their <br /> 
+                                    user stories and share yours</p>
+                                    <button className="btn bg-danger text-light">More info</button>
+                         </div>
+                         <div className="col-lg-6">
+                                <img src={jenkins} alt="jenkins" />
+                         </div>
+                     </div>
+                    </div>
+                      
+                    </Carousel.Item>
+                  
+                </Carousel>
+            </div>
+        </div>
+    )
+}
+export default Slider;
