c692e140c0b0c676af2cba75921528267a3a3ddd
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-03-24 17:17:46 -0700

    data include table

--- user list/user-list/src/component/table/table.js
+++ user list/user-list/src/component/table/table.js
@@ -6,43 +6,50 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { userData } from "../userData/userData";
 
 function createData() {
-  return;
+    return;
 }
 
 
 
- function BasicTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table  sx={{ minWidth: 100 }} size="small"  aria-label="simple table">
-        <TableHead >
-          <TableRow >
-         
-            {/* <TableCell align="right">User Name</TableCell> */}
-            <TableCell ><h4>User Name</h4> </TableCell>
-            <TableCell align="right"><h4>User Name</h4> </TableCell>
-            <TableCell align="right"><h4>Email</h4> </TableCell>
-            <TableCell align="right"><h4>Roles</h4> </TableCell>
-            <TableCell align="right"><h4>Action</h4> </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-       
-            <TableRow >
-              <TableCell component="th" scope="row">
-                Zaid Ali
-              </TableCell>
-              <TableCell align="right">zaidali</TableCell>
-              <TableCell align="right">Admin</TableCell>
-              <TableCell align="right">Admin</TableCell>
-            </TableRow>
-          
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+function BasicTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 100 }} aria-label="simple table">
+                <TableHead >
+                    <TableRow >
+
+                        {/* <TableCell align="right">User Name</TableCell> */}
+                        <TableCell ><h4>User Name</h4> </TableCell>
+                        <TableCell align="right"><h4>User Name</h4> </TableCell>
+                        <TableCell align="right"><h4>Email</h4> </TableCell>
+                        <TableCell align="right"><h4>Roles</h4> </TableCell>
+                        <TableCell align="right"><h4>Action</h4> </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+
+                    {
+                        userData.map((item) => {
+                            return (<TableRow >
+                                <TableCell component="th" scope="row">
+                                    {item.name}
+                                </TableCell>
+                                <TableCell align="right">{item.email}</TableCell>
+                                <TableCell align="right">{item.roles}</TableCell>
+                                <TableCell align="right">Admin</TableCell>
+                            </TableRow>)
+                        })
+                    }
+
+
+
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
 
 export default BasicTable;
---
+++ user list/user-list/src/component/userData/userData.js
@@ -0,0 +1,19 @@
+
+
+export const userData =[
+    {
+        name:"Zaid Ali",
+        email:"zaidali60gb@gmail.com",
+        roles:"Admin",
+    },
+    {
+        name:"Waqar",
+        email:"abc@gmail.com",
+        roles:"Admin",
+    },
+    {
+        name:"Faisal",
+        email:"lalag60gb@gmail.com",
+        roles:"Admin",
+    },
+];
