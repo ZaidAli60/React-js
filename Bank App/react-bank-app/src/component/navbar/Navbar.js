d0eb6df16f29de4910edd5c2cc1a9f1db109a9c2
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-05-02 09:37:44 -0700

    navbar complete

--- Bank App/react-bank-app/src/component/navbar/Navbar.js
+++ Bank App/react-bank-app/src/component/navbar/Navbar.js
@@ -5,30 +5,33 @@ function Navbar() {
 
 
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-primary navbars ">
-                
-                <div className="container">
-                    <a className="navbar-brand text-white" href="#">Bank</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          
+
+            <nav className="navbar navbar-expand-lg navbar-light  bg-primary">
+                <div className="container ">
+                    <a className="navbar-brand flex-grow-1 text-white fs-2" href="#">Bank</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
+                    <div className="collapse navbar-collapse text-center " id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active text-white fs-5" aria-current="page" href="#">Dashboard</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-white fs-5 px-4" href="#">Accounts</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-white fs-5" href="#">Transactions</a>
+                            </li>
+                        </ul>
 
+                    </div>
                 </div>
-                <div className="collapse navbar-collapse me-2" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active text-white " aria-current="page" href="#">Dashboard</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-white" href="#">Accounts</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-white" href="#">Transaction</a>
-                        </li>
 
-                    </ul>
-                </div>
+
             </nav>
+
         </>
     )
 }
