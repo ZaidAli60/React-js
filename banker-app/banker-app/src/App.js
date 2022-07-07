61a37a8d8b730bcc4df5dc160aaf2ac20998420a
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-07-07 13:15:21 -0700

    ctime

--- banker-app/banker-app/src/App.js
+++ banker-app/banker-app/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Navbar from "./component/landingpage/navbar/Navbar";
+import Time from "./component/landingpage/time/Time";
 function App() {
   return (
     <div className="App">
+      <Time />
       <Navbar />
     </div>
   );
