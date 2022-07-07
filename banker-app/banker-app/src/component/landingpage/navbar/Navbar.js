597e288201dded8425480ad0e4201f1a7f6c3f1c
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-07-07 13:09:55 -0700

    complete navbar and sidebar

--- banker-app/banker-app/src/component/landingpage/navbar/Navbar.js
+++ banker-app/banker-app/src/component/landingpage/navbar/Navbar.js
@@ -10,82 +10,104 @@ function Navbar() {
             Banker
             <span>.</span>
           </a>
-          <button
+          <a
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+            style={{
+              border: "none",
+            }}
           >
             <span className="navbar-toggler-icon"></span>
-          </button>
-
+          </a>
           <div
-            className="collapse navbar-collapse  "
-            id="navbarSupportedContent"
+            className="offcanvas offcanvas-end"
+            tabIndex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
           >
-            <ul className="navbar-nav  mb-2 mb-lg-0 ms-auto px-4">
-              <li className="nav-item">
-                <a
-                  className="nav-link activecolor"
-                  aria-current="page"
-                  href="#"
-                >
-                  Home
-                </a>
-              </li>
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel"></h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav  mb-2 mb-lg-0 ms-auto px-4">
+                <li className="nav-item">
+                  <a
+                    className="nav-link activecolor"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Home
+                  </a>
+                </li>
 
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  About Us
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Blog
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Contact
-                </a>
-              </li>
-            </ul>
-            <button
-              className="btn fw-bold  text-white text-uppercase "
-              style={{
-                backgroundColor: "#fd7e14",
-                fontSize:"14px"
-              }}
-            >
-              Login
-            </button>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    About Us
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Blog
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Dashboard
+                  </a>
+                </li>
+              </ul>
+              <button
+                className="btn fw-bold  text-white text-uppercase "
+                style={{
+                  backgroundColor: "#fd7e14",
+                  fontSize: "14px",
+                }}
+              >
+                Login
+              </button>
+            </div>
           </div>
         </div>
       </nav>
