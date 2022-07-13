dc759c25dad450fed15835b1aefde0635ee5544c
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-07-13 02:39:02 -0700

    Meet Team update

--- banker-app/banker-app/src/component/landingpage/meet team/MeetTeam.js
+++ banker-app/banker-app/src/component/landingpage/meet team/MeetTeam.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./MeetTeam.css";
-import Pic1 from "../../../Assets/pic1.jpg";
 import MeetData from "./MeetTeamData";
 function MeetTeam() {
   return (
@@ -14,20 +13,16 @@ function MeetTeam() {
         {MeetData.map((item) => {
           return (
             <div className="col-lg-3 col-md-4 col-sm-6 col-xm-12 py-4">
-              {/* <div className="card"> */}
                 <img src={item.img} className="card-img-top" alt="pic" />
                 <div >
                   <h5 className="pt-3">{item.heading}</h5>
                   <p className="opacity-50">{item.des}</p>
                 </div>
-              {/* </div> */}
             </div>
           );
         })}
 
-        {/* <div className="col">
-          <img src={Pic1} alt="pic1" />
-        </div> */}
+
       </div>
     </div>
   );
