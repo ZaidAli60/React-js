de2db64bccc361c52ab13059e7c8d07bc26c05f9
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-07-17 03:02:20 -0700

    create a account form

--- banker-app/banker-app/src/component/dashboard/pages/addAccount/AddAccount.js
+++ banker-app/banker-app/src/component/dashboard/pages/addAccount/AddAccount.js
@@ -1,11 +1,16 @@
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { Box } from "@mui/system";
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 import React from "react";
 
 function AddAccount() {
@@ -32,21 +37,85 @@ function AddAccount() {
         </Button>
       </Box>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
+        <DialogTitle>Enter Account Details Below</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
+          <Grid container spacing={1}>
+            <Grid item md={6}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Full Name"
+                type="text"
+                fullWidth
+                variant="outlined"
+                size="small"
+              />
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="CNIC Number"
+                type="email"
+                fullWidth
+                variant="outlined"
+                size="small"
+              />
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Branch Code"
+                type="number"
+                fullWidth
+                variant="outlined"
+                size="small"
+              />
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Account Number"
+                type="number"
+                fullWidth
+                variant="outlined"
+                size="small"
+              />
+            </Grid>
+            <Grid item md={6}>
+              <FormControl fullWidth >
+                <InputLabel id="demo-simple-select-label">Choose Account</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  // value={age}
+                  label="Choose Account"
+                  // onChange={handleChange}
+                >
+                  <MenuItem value={10}>Curent Account</MenuItem>
+                  <MenuItem value={20}>Saving Account</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Initial Deposit"
+                type="number"
+                fullWidth
+                variant="outlined"
+                size="small"
+              />
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
