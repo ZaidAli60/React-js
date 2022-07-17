d7467dde630e95556f4c583fc05dbf55ddda9396
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-07-17 02:16:44 -0700

    import dialog in mui

--- banker-app/banker-app/src/component/dashboard/pages/addAccount/AddAccount.js
+++ banker-app/banker-app/src/component/dashboard/pages/addAccount/AddAccount.js
@@ -1,10 +1,59 @@
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import React from "react";
 
 function AddAccount() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <>
-      {/* <h1>AddAccount</h1> */}
-    </>
+    <Box sx={{ p: 3 }}>
+      <Box>
+        <Button
+          style={{
+            backgroundColor: "#fd7e14",
+            color: "white",
+          }}
+          onClick={handleClickOpen}
+        >
+          Add Account
+        </Button>
+      </Box>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here.
+            We will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
 }
 
